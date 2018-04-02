5f5087aea54409de5870afda8051aacdf00a13c8
david <lexuszhi1990@gmail.com>
2018-04-02 23:46:28 +0800

    update jupyter

--- backup/jupyter.py
+++ backup/jupyter.py
@@ -9,6 +9,10 @@ RUN sed "s/#c.NotebookApp.token = '<generated>'/c.NotebookApp.token = 'your-toke
 EXPOSE 8888
 CMD jupyter notebook --port=8888 --ip 0.0.0.0 --no-browser --allow-root
 
+ADD sources.list /etc/apt/sources.list
+RUN apt-get update
+RUN apt-get install python3-pip -y
+RUN pip3 install mxnet-cu90mkl numpy -i https://pypi.douban.com/simple/
 
 
 RUN apt-get clean && \
@@ -18,3 +22,19 @@ RUN apt-get clean && \
 ENV TENSORFLOW_VERSION 1.5.0
 RUN pip --no-cache-dir install \
     http://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-${TENSORFLOW_VERSION}-cp36-cp36m-linux_x86_64.whl
+
+
+python -m ipykernel.kernelspec
+
+[root@pydev pydev]# jupyter-kernelspec list
+Available kernels:
+  python2    /usr/lib/python2.7/site-packages/ipykernel/resources
+
+pip3 install ipykernel
+
+[root@pydev ~]# python3 -m ipykernel install --name python3 --display-name "Python3.5.2"
+Installed kernelspec python3 in /usr/local/share/jupyter/kernels/python3
+[root@pydev ~]# jupyter-kernelspec list
+Available kernels:
+  python2    /usr/lib/python2.7/site-packages/ipykernel/resources
+  python3    /usr/local/share/jupyter/kernels/python3
